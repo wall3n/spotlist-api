533a753f078505f5466767ba604edc2dddb56082
wall3n <franmoreno1506@gmail.com>
2022-03-22 12:16:47 +0100

    test: testing the authenticated conexion

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -1,7 +1,25 @@
-const chai = require('chai');
+const chai = require("chai")
+const chaiHttp = require("chai-http")
+const api = "http://localhost:4000"
 
-describe('Main test', function() {
-    it('should return 1', function() {
-        chai.expect(1).to.equal(1);
+chai.use(chaiHttp)
+
+describe('Post a song', () => {
+    it('should return 401 when user is unauthenticated', (done) => {
+        chai.request(api)
+            .post("/users/1/lists/5/songs")
+            .send({
+                name: "powerlist",
+                description: "My playlist",
+                songs: ["Oh mama", "mama mia"]
+            })
+            .end((err, res) => {
+                if(err){
+                    throw err
+                }
+                chai.expect(res).to.have.status(401)
+                done()
+            })
+             
     });
 });
