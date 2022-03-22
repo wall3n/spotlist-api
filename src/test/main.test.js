169e86d80283a7297833fb9be76d2d7b63268d63
wall3n <franmoreno1506@gmail.com>
2022-03-22 23:58:01 +0100

    test: testing a song to be posted and receive it

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -7,12 +7,7 @@ chai.use(chaiHttp)
 describe('Post a song', () => {
     it('should return 401 when user is unauthenticated', (done) => {
         chai.request(api)
-            .post("/users/1/lists/5/songs")
-            .send({
-                name: "powerlist",
-                description: "My playlist",
-                songs: ["Oh mama", "mama mia"]
-            })
+            .post("/users/1/lists/")
             .end((err, res) => {
                 if(err){
                     throw err
@@ -20,6 +15,37 @@ describe('Post a song', () => {
                 chai.expect(res).to.have.status(401)
                 done()
             })
-             
     });
+    it('should return 401 if the userId does not exist', () => {
+        chai.request(api)
+            .post("/users/1/lists/")
+            .auth("Jhon Smith", "unsecuredpassword1234")
+            .end((err, res) => {
+                if(err) throw err
+                chai.expect(res).to.have.status(401)
+            })
+
+    })
+    it('should send 200 when a list is uploaded and load the list', (done) => {
+        chai.request(api)
+            .post("/users/aaaa00/lists/")
+            .auth('Jhon Smith', 'unsecuredpassword1234')
+            .send({
+                name: "My list",
+                description: "Nice description",
+                songs: ["Malamente", "Saoko"]
+            })
+            .end((err, res) => {
+                if(err) throw err
+                chai.expect(res).to.have.status(200)
+                chai.request(api)
+                    .get("/users/aaaa00/lists")
+                    .auth("Jhon Smith", "unsecuredpassword1234")
+                    .end((err, res) => {
+                        if(err) throw err
+                        chai.expect(res.body[0]).to.have.property("name").to.be.equal("My list")
+                        done()
+                    })
+            })
+    })
 });
