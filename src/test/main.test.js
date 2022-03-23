453534b92b689fac5c1de3edb57d487c234ee4c1
wall3n <franmoreno1506@gmail.com>
2022-03-23 17:42:41 +0100

    test: teting searching a list and adding a song

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -4,7 +4,7 @@ const api = "http://localhost:4000"
 
 chai.use(chaiHttp)
 
-describe('Post a song', () => {
+describe('Post a list and load it', () => {
     it('should return 401 when user is unauthenticated', (done) => {
         chai.request(api)
             .post("/users/1/lists/")
@@ -28,8 +28,8 @@ describe('Post a song', () => {
     })
     it('should send 200 when a list is uploaded and load the list', (done) => {
         chai.request(api)
-            .post("/users/aaaa00/lists/")
-            .auth('Jhon Smith', 'unsecuredpassword1234')
+            .post("/users/aaaa01/lists/")
+            .auth('Laurel Fitz', 'abcdef1234')
             .send({
                 name: "My list",
                 description: "Nice description",
@@ -39,8 +39,8 @@ describe('Post a song', () => {
                 if(err) throw err
                 chai.expect(res).to.have.status(200)
                 chai.request(api)
-                    .get("/users/aaaa00/lists")
-                    .auth("Jhon Smith", "unsecuredpassword1234")
+                    .get("/users/aaaa01/lists")
+                    .auth("Laurel Fitz", "abcdef1234")
                     .end((err, res) => {
                         if(err) throw err
                         chai.expect(res.body[0]).to.have.property("name").to.be.equal("My list")
@@ -48,4 +48,35 @@ describe('Post a song', () => {
                     })
             })
     })
+    it('should load a list by its id', (done) => {
+        chai.request(api)
+            .get('/users/aaaa00/lists/1')
+            .auth('Jhon Smith', "unsecuredpassword1234")
+            .end((err, res) => {
+                if(err) throw err
+                chai.expect(res.body).to.have.property("name").to.be.equal("Example")
+                done()
+            })
+    })
 });
+
+describe('post a song', () => {
+    it('should post a song if the user and the listid is correct' ,(done) => {
+        chai.request(api)
+            .post('/users/aaaa00/lists/1/songs')
+            .auth('Jhon Smith', 'unsecuredpassword1234')
+            .send({ song: "Cayo la noche remix" })
+            .end((err, res) => {
+                if(err) throw err
+                chai.expect(res).to.have.status(200)
+                chai.request(api)
+                    .get('/users/aaaa00/lists/1')
+                    .auth('Jhon Smith', 'unsecuredpassword1234')
+                    .end((err, res) => {
+                        if(err) throw err
+                        chai.expect(res.body.songs[-1]).to.be.equal("Cayo la noche remix")
+                        done()
+                    })
+            })
+    })
+})
